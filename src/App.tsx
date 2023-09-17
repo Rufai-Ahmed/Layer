a188a5b789c2223b4debe9fa6f2b861bbd0bc651
Rufai-Ahmed <abbeyrufai234@gmail.com>
2023-09-17 19:01:58 +0100

    Layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 import styled from "styled-components";
 import { RouterProvider } from "react-router-dom";
 import { MainRouter } from "./Router/MainRouter";
+import loading from "./Asset/loading.gif";
 
 function App() {
+  const ShowRef: any = useRef();
+  const loadRef: any = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      loadRef.current.style.display = "none";
+      ShowRef.current.style.display = "block";
+    }, 3000);
+  }, [RouterProvider]);
+
   return (
     <div>
-      <RouterProvider router={MainRouter} />
+      <Container ref={loadRef}>
+        <img src={loading} alt="" />
+      </Container>
+      <Hold ref={ShowRef}>
+        <RouterProvider router={MainRouter} />
+      </Hold>
     </div>
   );
 }
 
 export default App;
+
+const Hold = styled.div`
+  display: none;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
